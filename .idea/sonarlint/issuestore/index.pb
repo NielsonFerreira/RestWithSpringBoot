4dc67252cf62142eb4e1d54232c144998ff4fa58
Nielson Ferreira <nielsonfferreira@hotmail.com>
2021-08-22 14:09:44 -0300

    Implementando HATEOAS

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,4 @@
 
-:
-
-.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
 |
 Lsrc/main/java/br/com/nielsonferreira/request/converters/NumberConverter.java,4\7\4720e7892ad0d9dc0cf833aaac0669427b24d1ae
 i
@@ -12,9 +9,3 @@ Esrc/main/java/br/com/nielsonferreira/repository/PersonRepository.java,c\7\c7ee
 Msrc/main/java/br/com/nielsonferreira/exception/ResourceNotFoundException.java,3\6\36e3ea6bec815185bf41297cd11414fd191c0cfa
 k
 ;src/main/java/br/com/nielsonferreira/data/model/Person.java,7\a\7a786a7a62503bfb35130eb10a805d64fabff129
-q
-Asrc/main/java/br/com/nielsonferreira/services/PersonServices.java,2\8\282b982827fe2d8ea450e34d0a9a279e242def0d
-v
-Fsrc/test/java/br/com/nielsonferreira/converter/DozerConverterTest.java,e\0\e04b54f7d75e33eade5344341084af0c5b01e473
-t
-Dsrc/test/java/br/com/nielsonferreira/converter/mocks/MockPerson.java,0\c\0c1ebb05bd6b2796445ea931bd728308a91f5c01
--- src/main/java/br/com/nielsonferreira/data/vo/PersonVO.java
+++ src/main/java/br/com/nielsonferreira/data/vo/PersonVO.java
@@ -1,15 +1,19 @@
 package br.com.nielsonferreira.data.vo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+import org.springframework.hateoas.ResourceSupport;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-public class PersonVO implements Serializable {
+@JsonPropertyOrder({ "id", "firstName", "lastName", "address", "gender" })
+public class PersonVO extends ResourceSupport implements Serializable {
 
-    private Long id;
+    @Mapping("id")
+    @JsonProperty("id")
+    private Long key;
     private String firstName;
     private String lastName;
     private String address;
@@ -18,12 +22,12 @@ public class PersonVO implements Serializable {
     public PersonVO() {
     }
 
-    public Long getId() {
-        return id;
+    public Long getKey() {
+        return key;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setKey(Long key) {
+        this.key = key;
     }
 
     public String getFirstName() {
@@ -62,12 +66,13 @@ public class PersonVO implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         PersonVO personVO = (PersonVO) o;
-        return Objects.equals(id, personVO.id) && Objects.equals(firstName, personVO.firstName) && Objects.equals(lastName, personVO.lastName) && Objects.equals(address, personVO.address) && Objects.equals(gender, personVO.gender);
+        return Objects.equals(key, personVO.key) && Objects.equals(firstName, personVO.firstName) && Objects.equals(lastName, personVO.lastName) && Objects.equals(address, personVO.address) && Objects.equals(gender, personVO.gender);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, address, gender);
+        return Objects.hash(super.hashCode(), key, firstName, lastName, address, gender);
     }
 }
